6752c212fe106cc5b7a28bf91d5e3432677aa803
Jinnerz <chuayongjin@gmail.com>
2025-02-13 13:07:35 +0800

    Debugging

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,7 @@
 require('dotenv').config()
 
+const cors = require("cors");
+app.use(cors());
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
